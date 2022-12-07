289beaf72810821dc5cecebbc5006b044deeef2c
Huu Hoa NGUYEN <huuhoa@gmail.com>
2022-12-07 09:28:11 +0700

    chg: support loading templates, assets relatively (#3)
    Allow to run md2pdf.py from anywhere

--- src/md2pdf.py
+++ src/md2pdf.py
@@ -2,58 +2,93 @@
 md2pdf: render markdown file to pdf
 """
 
-def markdown_transform(src) -> dict:
-	import frontmatter # https://pypi.org/project/python-frontmatter/
+def markdown_to_html(content: str) -> str:
+	"""
+	transform markdown to html
+	"""
 	import markdown
+	parser = markdown.Markdown(extensions=['codehilite', 'fenced_code', 'toc'])
+	return parser.convert(content)
+
+
+def markdown_preprocessing(src) -> dict:
+	"""
+	Preprocessing the markdown file.
+
+	Extract the Front Matter header to save into metadata key
+	Remaining content is in the markdown_content key
+	"""
+
+	import frontmatter # https://pypi.org/project/python-frontmatter/
+	from pathlib import Path
 
 	file_parts = frontmatter.load(src)
-	parser = markdown.Markdown(extensions=['codehilite', 'fenced_code', 'toc'])
+	p = Path(__file__).parent.parent
+	file_parts.metadata['STATIC_URL'] = f'{p}'
+
 	return {
-		'html_content': parser.convert(file_parts.content),
+		'markdown_content': file_parts.content,
 		'metadata': file_parts.metadata,
 	}
 
 
-def html_transform(data_dict, template='template.html') -> str:
+def html_transform(data_dict: dict, layout_template: str) -> str:
+	"""
+	Render data with jinja2 template
+	"""
 	import jinja2
-	env = jinja2.Environment(loader=jinja2.FileSystemLoader("./templates"))
-	tmpl = env.get_template(template)
+	from pathlib import Path
+	p = Path(__file__).parent.parent / 'templates'
+	env = jinja2.Environment(loader=jinja2.FileSystemLoader(p))
+	tmpl = env.get_template(layout_template)
 	data = tmpl.render(data_dict)
 	return data
 
-def main():
+
+def pdf_render(html, dest_file, metadata):
 	from io import StringIO
-	import argparse
 	import xhtml2pdf.pisa as pisa
 
-	parser = argparse.ArgumentParser("md2pdf", description='Build PDF eBook from markdown files')
-	parser.add_argument('--input', '-i', type=str, help='Markdown input')
-	parser.add_argument('--output', '-o', type=str, help='PDF eBook output')
-	parser.add_argument('--template', '-t', type=str, default='', help='Template file for rendering html')
-	parser.add_argument('--trace', type=bool, default=False, help='When enable, will render html to a trace.html file for debugging purpose')
-
-	args = parser.parse_args()
-	
-	md_data = markdown_transform(args.input)
-
-	template = f"{md_data['metadata'].get('layout', args.template)}.html"
-	data = html_transform(md_data, template)
-	
-	if args.trace:
-		with open('trace.html', 'wt') as file_dest:
-			file_dest.write(data)
-	
 	# Shortcut for dumping all logs to the screen
 	pisa.showLogging()
-	with open(args.output, 'wb') as pdf_file:
-		pdf = pisa.CreatePDF(StringIO(data), pdf_file, context_meta={
-			'author': md_data['metadata'].get('author', ''),
-			'subject': md_data['metadata'].get('subject', ''),
-			'title': md_data['metadata'].get('title', ''),
+	with open(dest_file, 'wb') as pdf_file:
+		pdf = pisa.CreatePDF(StringIO(html), pdf_file, context_meta={
+			'author': metadata.get('author', ''),
+			'subject': metadata.get('subject', ''),
+			'title': metadata.get('title', ''),
 		})
 
 	if pdf.err:
 		print(f"*** {pdf.err} ERRORS OCCURRED")
+
+
+def main():
+	import argparse
+
+	parser = argparse.ArgumentParser("md2pdf", description='Build PDF eBook from markdown files')
+	parser.add_argument('--input', '-i', type=str, help='Markdown input')
+	parser.add_argument('--output', '-o', type=str, help='PDF eBook output')
+	parser.add_argument('--layout', '-l', type=str, default='', help='Layout template to render html to final form')
+	parser.add_argument('--trace', type=bool, default=False, help='When enable, will render html to a trace.html file for debugging purpose')
+
+	args = parser.parse_args()
+	
+	md_data = markdown_preprocessing(args.input)
+	metadata = md_data['metadata']
+	layout = metadata.get('layout', args.layout)
+	if not layout:
+		print(f"*** Must specify the layout, either in front matter of md file or in command line argument (--layout)")
+		return
+	layout = f"{layout}.html"
+
+	md_data['html_content'] = markdown_to_html(md_data['markdown_content'])
+	html = html_transform(md_data, layout)
+	
+	if args.trace:
+		with open('trace.html', 'wt') as file_dest:
+			file_dest.write(html)
+
+	pdf_render(html, args.output, metadata)	
 
 if __name__ == "__main__":
 	main()
